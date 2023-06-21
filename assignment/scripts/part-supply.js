afb708646819b8edb770845ac227f6fc2e616dea
Gabrielle Lepensky <gabrielle.lepensky@gmail.com>
2023-06-21 01:12:38 -0500

    finished all but last stretch goal

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -50,9 +50,26 @@ console.log("---  Stretch Goals  ---");
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
 
+console.log(supplyChanges);
+
+for (let count of supplyChanges) {
+  if (count > 0) {
+    console.log("Added " + count + " parts.");
+  } else if (count < 0) {
+    console.log("Removed " + count + " parts.");
+  } else {
+    console.log("No change.");
+  }
+}
+
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log("8. Total supplies available is:");
+let sum = 0;
+for (i = 0; i< supplyChanges.length; i++){
+  sum += supplyChanges[i];
+}
+console.log(sum);
 
 // 9. We have a large stash of parts in our warehouse that we
 //    need to box up and get ready for shipment.
@@ -61,3 +78,15 @@ console.log("8. Total supplies available is:");
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+let partsTotal = 572;
+let eachBox = 7;
+
+while (partsTotal / eachBox == filledBox){
+  
+}
+
+
+
+
+
+
